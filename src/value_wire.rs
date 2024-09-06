9d09855f90e0a923c8c199f70a1aae11e31ebbb0
Andrew Morris <voltrevo@gmail.com>
2024-09-06 15:38:44 +1000

    add subtraction, multiplication

--- src/value_wire.rs
+++ src/value_wire.rs
@@ -6,6 +6,7 @@ use crate::{
     id_generator::IdGenerator,
 };
 
+#[derive(Clone)]
 pub struct ValueWire {
     pub id_gen: Rc<RefCell<IdGenerator>>,
     pub bits: Vec<Rc<BoolWire>>,
@@ -32,6 +33,24 @@ impl ValueWire {
         // significant bit first. This way we expose a big endian interface but can use little
         // endian internally which simplifies the code.
         bits.reverse();
+
+        ValueWire {
+            id_gen: id_gen.clone(),
+            bits,
+        }
+    }
+
+    pub fn new_const(mut value: u64, id_gen: &Rc<RefCell<IdGenerator>>) -> Self {
+        let mut bits = Vec::new();
+
+        while value > 0 {
+            bits.push(Rc::new(BoolWire {
+                id_gen: id_gen.clone(),
+                data: BoolData::Const(value & 1 == 1),
+            }));
+
+            value >>= 1;
+        }
 
         ValueWire {
             id_gen: id_gen.clone(),
@@ -66,10 +85,8 @@ impl ValueWire {
 
             let sum = BoolWire::xor(&a_bit, &b_bit);
 
-            let new_carry = BoolWire::or(
-                &BoolWire::and(&a_bit, &b_bit),
-                &BoolWire::and(&carry, &BoolWire::xor(&a_bit, &b_bit)),
-            );
+            let new_carry =
+                BoolWire::or(&BoolWire::and(&a_bit, &b_bit), &BoolWire::and(&carry, &sum));
 
             bits.push(BoolWire::xor(&sum, &carry));
             carry = new_carry;
@@ -79,5 +96,102 @@ impl ValueWire {
             id_gen: a.id_gen.clone(),
             bits,
         }
+    }
+
+    pub fn bit_not(a: &ValueWire) -> ValueWire {
+        let bits = a.bits.iter().map(|bit| BoolWire::not(bit)).collect();
+
+        ValueWire {
+            id_gen: a.id_gen.clone(),
+            bits,
+        }
+    }
+
+    pub fn negate(&self) -> ValueWire {
+        ValueWire::add(
+            &ValueWire::bit_not(self),
+            &ValueWire::new_const(1, &self.id_gen),
+        )
+    }
+
+    pub fn resize(&self, size: usize) -> ValueWire {
+        ValueWire {
+            id_gen: self.id_gen.clone(),
+            bits: (0..size).map(|i| self.at(i)).collect(),
+        }
+    }
+
+    pub fn sub(a: &ValueWire, b: &ValueWire) -> ValueWire {
+        let neg_b = if b.bits.len() < a.bits.len() {
+            ValueWire::negate(&b.resize(a.bits.len()))
+        } else {
+            ValueWire::negate(b)
+        };
+
+        ValueWire::add(a, &neg_b)
+    }
+
+    pub fn shift_up_const(&self, amount: usize) -> ValueWire {
+        let mut bits = Vec::with_capacity(self.bits.len());
+
+        for _ in 0..amount {
+            bits.push(Rc::new(BoolWire {
+                id_gen: self.id_gen.clone(),
+                data: BoolData::Const(false),
+            }));
+        }
+
+        for i in 0..self.bits.len() - amount {
+            bits.push(self.bits[i].clone());
+        }
+
+        ValueWire {
+            id_gen: self.id_gen.clone(),
+            bits,
+        }
+    }
+
+    pub fn mul_bool(a: &Rc<BoolWire>, b: &ValueWire) -> ValueWire {
+        let mut bits = Vec::with_capacity(b.bits.len());
+
+        for i in 0..b.bits.len() {
+            bits.push(BoolWire::and(a, &b.bits[i]));
+        }
+
+        ValueWire {
+            id_gen: b.id_gen.clone(),
+            bits,
+        }
+    }
+
+    pub fn mul(a: &ValueWire, b: &ValueWire) -> ValueWire {
+        let size = std::cmp::max(a.bits.len(), b.bits.len());
+
+        let (sm, lg) = if a.bits.len() <= b.bits.len() {
+            (a, b)
+        } else {
+            (b, a)
+        };
+
+        let mut sum_terms = Vec::new();
+
+        for i in 0..sm.bits.len() {
+            let term = ValueWire::mul_bool(&sm.bits[i], &lg.shift_up_const(i));
+            sum_terms.push(term);
+        }
+
+        tree_sum(&sum_terms)
+    }
+}
+
+fn tree_sum(values: &[ValueWire]) -> ValueWire {
+    if values.len() == 1 {
+        values[0].clone()
+    } else {
+        let mid = values.len() / 2;
+        let left = tree_sum(&values[..mid]);
+        let right = tree_sum(&values[mid..]);
+
+        ValueWire::add(&left, &right)
     }
 }
